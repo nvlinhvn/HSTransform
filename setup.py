87b0cfd9b5293ed77fa9c322ae6335c90c90d414
nvlinhvn <nguyenvietlinh.eee@gmail.com>
2024-05-10 00:04:43 +0200

    Update package information
    to align with Pypi standard

--- setup.py
+++ setup.py
@@ -8,15 +8,20 @@ and the Wavelet Transform.
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='HSTransform',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     license='MIT',
     author='Linh V Nguyen',
     author_email='linhvietnguyen.ee@gmail.com',
     description='A Package to Compute S-transform with Hyperbolic Window',
     python_requires='>=3.10',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=[
         'numpy',
         'scipy',
